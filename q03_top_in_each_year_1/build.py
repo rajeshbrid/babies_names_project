3acfc87600585e6bc68fd2d102cf2eebc95eb1e1
rajeshbrid <rajesh_brid@hotmail.com>
2018-11-02 04:44:48 +0000

    Done

--- q03_top_in_each_year_1/build.py
+++ q03_top_in_each_year_1/build.py
@@ -1,9 +1,21 @@
+# %load q03_top_in_each_year_1/build.py
 
 import pandas as pd
 
 path = 'data/babies_name.csv'
 data = pd.read_csv(path,names=['Name', 'Gender', 'Count', 'Year'])
 
-def q03_top_in_each_year_1():
-    "write your solution here"
+'write your solution here'
+
+def q03_top_in_each_year_1(data):
+    dictionary = {'Name':data['Name'],
+                 'Count':data['Count'],
+                 'Year':data['Year']}
+    # print (dictionary)    
+    return dictionary
+
+# q03_top_in_each_year_1(data)
+
+
+
 
